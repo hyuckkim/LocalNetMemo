406d5179d7500bfbebbe5193a43648ebc74cfac8
monkeyhyuck@gmail.com <monkeyhyuck@gmail.com>
2020-08-20 08:27:39 +0900

    multi channal update

--- input.php
+++ input.php
@@ -1,5 +1,9 @@
 <?php
-  $newfile = "file.txt";
+  if($_POST['url'] != "")
+    $newfile = "texts/" . $_POST['url'] . ".txt";
+  else
+    $newfile = "file.txt";
+
   file_put_contents($newfile, $_POST['str']);
   echo $_POST['str'];
 ?>
--- output.php
+++ output.php
@@ -1,6 +1,10 @@
 <?php
-//일반적인 파일 읽기 사용 예
-$file_handle = fopen('file.txt', "r");
+//일반적인 파일 읽기 사용 예 
+  if($_POST['url'] != "")
+    $newfile = "texts/" . $_POST['url'] . ".txt";
+  else
+    $newfile = "file.txt";
+$file_handle = fopen($newfile, "r");
 
     while (!feof($file_handle)) {
 
